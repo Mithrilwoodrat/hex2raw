bf6f5708a0010e03d4123e62fe806822fb5f2331
Mithrilwoodrat <mithrilwoodrat@gmail.com>
2014-11-22 20:27:17 +0800

    Update hex2raw.py

--- hex2raw.py
+++ hex2raw.py
@@ -1,2 +1,8 @@
 #author : mithrilwoodrat
-reduce(lambda x,y:x+y,[chr(int(i,16)) for i in s.split()])
+# -*- coding: utf-8 -*-
+import sys
+
+
+sys.out.write(reduce(lambda x,y:x+y,
+                     [chr(int(i,16)) for i in
+                      sys.stdin.readline().split()])
